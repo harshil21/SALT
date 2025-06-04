4caf7ed7d0dbe85f20a73c73c9c491f7be129afe
Harshil <37377066+harshil21@users.noreply.github.com>
2025-06-03 21:54:08 -0400

    code to transmit data over serial

--- src/main.rs
+++ src/main.rs
@@ -1,17 +1,34 @@
-use std::process::Command;
+use std::process::{Command, Stdio};
 use bmp280::Bmp280Builder;
 use std::thread;
 use bno055;
 use linux_embedded_hal::I2cdev;
+use std::time::Duration;
+use std::io::Write;
+
+struct TransmittedPacket {
+    pressure: f32,
+    altitude: f32,
+    temperature: f32,
+    orientation: (f32, f32, f32), // (roll, pitch, yaw)
+}
+
 
 fn main() {
     // Start new thread for pressure sensor:
-    run_sensors();
+    thread::spawn(|| {
+        run_sensors();
+    });
+    thread::spawn(|| {
+        transmit_data();
+    });
+    record_video();
 }
 
 fn record_video() {
     let status = Command::new("rpicam-vid")
-        .args(&["-t", "5000", "-o", "test.h264", "--inline", "--awb", "auto", "--width", "1920", "--height", "1080"])
+        .args(&["-t", "10000", "-o", "test.h264", "--inline", "--awb", "auto", "--width", "1920", "--height", "1080"])
+        .stdout(Stdio::null())
         .status()
         .expect("failed to execute camera command");
 
@@ -22,15 +39,40 @@ fn record_video() {
     }
 }
 
+fn transmit_data() {
+    // Open the serial port with desired settings
+    loop {
+        let mut port = serialport::new("/dev/serial0", 9600)
+            .timeout(Duration::from_millis(1000))
+            .open()
+            .expect("Failed to open port");
+
+        // Data to send
+        let output = b"Hello, serial world!\n";
+
+        // Write data to the serial port
+        port.write_all(output).expect("Write failed!");
+
+        thread::sleep(Duration::from_millis(1000));
+    }
+
+}
+
 fn run_sensors() {
     let mut bmp280 = init_pressure_sensor();
     let mut sensor = init_orientation_sensor();
-
+    let mut transmitted_packet = TransmittedPacket {
+        pressure: 0.0,
+        altitude: 0.0,
+        temperature: 0.0,
+        orientation: (0.0, 0.0, 0.0),
+    };
 
     loop {
         // Pressure
         if let Ok(pressure) = bmp280.pressure_kpa() {
             println!("{:?} kPa", pressure);
+            transmitted_packet.pressure = pressure;
         } else {
             println!("Read/write error");
         }
@@ -38,6 +80,7 @@ fn run_sensors() {
         // Altitude
         if let Ok(altitude) = bmp280.altitude_m() {
             println!("{:?} m", altitude);
+            transmitted_packet.altitude = altitude;
         } else {
             println!("Read/write error");
         }
@@ -45,6 +88,7 @@ fn run_sensors() {
         // Temperature
         if let Ok(temp) = bmp280.temperature_celsius() {
             println!("{:?} °C", temp);
+            transmitted_packet.temperature = temp;
         } else {
             println!("Read/write error");
         }
@@ -53,6 +97,7 @@ fn run_sensors() {
         match sensor.euler_angles() {
             Ok(euler) => {
                 println!("Orientation: {:?}", euler);
+                transmitted_packet.orientation = (euler.a, euler.b, euler.c);
             }
             Err(e) => {
                 eprintln!("Error reading orientation: {:?}", e);
